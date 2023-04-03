1c031f2e94210d69b22b1697b30b55231a150a98
Chloe <cguttweb@pm.me>
2023-04-03 21:28:23 +0100

    vue single file component basics

---
+++ vue-sfcs/src/components/MyUsername.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p class="username">{{ username }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  username: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<style lang="css">
+.username {
+  color: #dcae1d;
+  font-size: 4rem;
+}
+</style>
