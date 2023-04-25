a23ba85573c6e0a3196cd8180d1051675c6e9670
Chloe <cguttweb@pm.me>
2023-04-25 20:52:25 +0100

    example using local & session storage

---
+++ vueuse-app/src/components/VueUseStorage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>useLocalStorage and useSessionStorage</p>
+    <label for="name">Name</label>
+    <input type="text" name="name" v-model="name" />
+    <label for="favfood">Favourite Food:</label>
+    <input type="text" name="favfood" v-model="myFavFood" />
+  </div>
+</template>
+
+<script setup>
+import { useLocalStorage, useSessionStorage } from '@vueuse/core'
+const name = useLocalStorage('name', 'Chloe')
+const myFavFood = useSessionStorage('favfood', 'pasta')
+</script>
+
+<style></style>
