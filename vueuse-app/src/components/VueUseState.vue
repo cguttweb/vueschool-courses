ee89f25f3675b764d68dde6f5e7d4a25b120d472
Chloe <cguttweb@pm.me>
2023-04-25 20:37:54 +0100

    added example of using refhistory to track state

--- vueuse-app/src/components/VueUseState.vue
+++ vueuse-app/src/components/VueUseState.vue
@@ -1,27 +1,25 @@
 <template>
-  <div>
+  <div style="padding-top: 20px">
     <!-- <label for="name">Name:</label>
     <input type="text" name="name" id="name" v-model="name" /> -->
-    <!-- prettierignore -->
     <input
       type="text"
-      name="todo"
-      id="todo"
+      name="todoitem"
       placeholder="Add New Todo Item"
-      v-model="newToDo"
+      v-model="newTodo"
     />
     <button
-      @click="
-        todos.unshift()
-        newToDo = ''
-      "
-    >
+      @click="todos.unshift(newTodo);  
+        newTodo = '';">
       Add New ToDo
     </button>
 
     <button @click="undo">Undo</button>
     <button @click="redo">Redo</button>
 
+    <ul>
+      <li v-for="todo in todos" :key="todo">{{ todo }}</li>
+    </ul>
     <pre>{{ history }}</pre>
   </div>
 </template>
@@ -30,14 +28,21 @@
 import { useRefHistory, useDebouncedRefHistory } from '@vueuse/core'
 import { ref } from 'vue'
 
-const newToDo = ref('')
-const toDos = ref([])
+const newTodo = ref('')
+const todos = ref([])
 // const { history, undo, redo } = useRefHistory(name)
 
 // const { history, undo, redo } = useDebouncedRefHistory(name, {
 //   // number of milliseconds to delay before new entry
 //   debounce: 1000,
 // })
+
+const { history, undo, redo } = useRefHistory(todos, {
+  // this is needed for use with arrays and objects to track teir history
+  deep: true,
+  // to limit history
+  capacity: 15
+})
 </script>
 
 <style scoped></style>
